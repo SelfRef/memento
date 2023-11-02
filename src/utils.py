8c6b86f8d0c148525d4d5bc8e477c695b3b920ca
SelfRef <git@selfref.dev>
2023-11-02 12:05:01 +0100

    Fix if

--- src/utils.py
+++ src/utils.py
@@ -26,9 +26,8 @@ def get_list_of_files(path: str, types: None | List[str] = None) -> List[str]:
 			file_path = os.path.join(root, file)
 			if (types is None):
 				file_paths.append(file_path)
-			else:
-				if os.path.splitext(file_path)[1] in types:
-					file_paths.append(file_path)
+			elif os.path.splitext(file_path)[1] in types:
+				file_paths.append(file_path)
 	return file_paths
 
 def scan_ocr_image(path: str, callback: Callable[[], List[str]]):
